6c996b642507fb1fd1cb3ed2afdaabf65a26abf3
Harsh Rastogi <68362699+Kota-Kun22@users.noreply.github.com>
2024-12-13 23:37:19 +0530

    changed from object to data object

--- app/src/main/java/com/example/anime_handbook/navigation/Screen.kt
+++ app/src/main/java/com/example/anime_handbook/navigation/Screen.kt
@@ -1,17 +1,17 @@
 package com.example.anime_handbook.navigation
 
  sealed class Screen(val route:String) {
-     object Splash : Screen("splash_screen")
-     object Welcome : Screen("welcome_screen")
-     object Home : Screen("home_screen")
-     object Details : Screen("details_screen/{heroId}") //required and optional arguments
+     data object Splash : Screen("splash_screen")
+     data object Welcome : Screen("welcome_screen")
+     data object Home : Screen("home_screen")
+     data object Details : Screen("details_screen/{heroId}") //required and optional arguments
      {
          fun passHeroId(heroId:Int):String
          {
              return "details_screen/$heroId"
          }
      }
-     object Search : Screen("search_screen")
+     data object Search : Screen("search_screen")
 
 
  }
