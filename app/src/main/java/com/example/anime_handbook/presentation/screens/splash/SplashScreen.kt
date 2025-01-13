b406879d5936c4a663b74e8851a7f3046bd616da
Harsh Rastogi <68362699+Kota-Kun22@users.noreply.github.com>
2025-01-13 12:04:45 +0530

    Splash Screen animation is completed Splash Screen completed !!!

--- app/src/main/java/com/example/anime_handbook/presentation/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/example/anime_handbook/presentation/screens/splash/SplashScreen.kt
@@ -3,14 +3,20 @@ package com.example.anime_handbook.presentation.screens.splash
 import android.content.res.Configuration
 import android.window.SplashScreenView
 import androidx.collection.emptyLongSet
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.TweenSpec
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -21,14 +27,26 @@ import androidx.navigation.NavHostController
 import com.example.anime_handbook.R
 import com.example.anime_handbook.ui.theme.Purple500
 import com.example.anime_handbook.ui.theme.Purple700
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
-    Splash()
+    val rotate= remember{Animatable(0f)}
+
+    LaunchedEffect(key1 = true) {
+        rotate.animateTo(
+            targetValue = 360f,
+            animationSpec = tween(durationMillis=1000, delayMillis = 200)
+        )
+    }
+
+
+
+    Splash(rotate= rotate.value)
 }
 
 @Composable
-fun Splash()
+fun Splash(rotate:Float)
 {
     if(isSystemInDarkTheme())
     {
@@ -37,7 +55,11 @@ fun Splash()
             .fillMaxSize(),
             contentAlignment = Alignment.Center
         ){
-            Image(painter= painterResource(id =R.drawable.logo), contentDescription = stringResource(R.string.app_logo))
+            Image(
+                modifier= Modifier.rotate(degrees = rotate),
+                painter= painterResource(id =R.drawable.logo),
+                contentDescription = stringResource(R.string.app_logo)
+            )
 
         }
 
@@ -49,7 +71,10 @@ fun Splash()
             .fillMaxSize(),
             contentAlignment = Alignment.Center
         ){
-            Image(painter= painterResource(id =R.drawable.logo), contentDescription = stringResource(R.string.app_logo))
+            Image(
+                modifier= Modifier.rotate(degrees = rotate),
+                painter= painterResource(id =R.drawable.logo),
+                contentDescription = stringResource(R.string.app_logo))
 
         }
 
@@ -64,7 +89,7 @@ fun Splash()
 @Preview
 @Composable
 fun SplashScreenPreview() {
-    Splash()
+    Splash(rotate = 0f)
     
 }
 
@@ -72,5 +97,5 @@ fun SplashScreenPreview() {
 @Composable
 fun SplashScreenView_DarkTheme()
 {
-    Splash()
+    Splash(rotate = 0f)
 }
