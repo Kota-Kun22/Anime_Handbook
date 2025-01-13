1e871b10fc4646d0cd1ba0fd91d7ed83357780a3
Harsh Rastogi <68362699+Kota-Kun22@users.noreply.github.com>
2025-01-13 11:47:38 +0530

    Splash Screen light and dark mode are completed !

--- app/src/main/java/com/example/anime_handbook/presentation/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/example/anime_handbook/presentation/screens/splash/SplashScreen.kt
@@ -1,13 +1,18 @@
 package com.example.anime_handbook.presentation.screens.splash
 
+import android.content.res.Configuration
+import android.window.SplashScreenView
+import androidx.collection.emptyLongSet
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,14 +30,34 @@ fun SplashScreen(navController: NavHostController) {
 @Composable
 fun Splash()
 {
-    Box(modifier= Modifier
-        .background(Brush.verticalGradient(listOf(Purple700, Purple500)))
-        .fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ){
-        Image(painter= painterResource(id =R.drawable.logo), contentDescription = stringResource(R.string.app_logo))
+    if(isSystemInDarkTheme())
+    {
+        Box(modifier= Modifier
+            .background(Color.Black)
+            .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ){
+            Image(painter= painterResource(id =R.drawable.logo), contentDescription = stringResource(R.string.app_logo))
+
+        }
 
     }
+    else
+    {
+        Box(modifier= Modifier
+            .background(Brush.verticalGradient(listOf(Purple700, Purple500)))
+            .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ){
+            Image(painter= painterResource(id =R.drawable.logo), contentDescription = stringResource(R.string.app_logo))
+
+        }
+
+    }
+
+
+
+
 
 }
 
@@ -41,4 +66,11 @@ fun Splash()
 fun SplashScreenPreview() {
     Splash()
     
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun SplashScreenView_DarkTheme()
+{
+    Splash()
 }
